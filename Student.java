7097ef602c7c1222f090f54328a8cc43f3e4dd3b
M-m777 <mahmoudmosalam2006@gmail.com>
2025-05-10 11:33:22 -0400

    student

--- Student.java
+++ Student.java
@@ -1,55 +1,33 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class  Student extends DMAP {
-    private String Major;
+public class Student extends DMAP {
+    private String major;
     private String gender;
-    public Student(){
 
-    }
-    public Student(String name, String Address, String Email, int ID, int age,String Major, String gender){
-        super(name, Address, Email, ID, age);
-        this.Major = Major;
+    public Student() {}
+
+    public Student(String name, String address, String email, int ID, int age, String major, String gender) {
+        super(name, address, email, ID, age);
+        this.major = major;
         this.gender = gender;
+    }
 
+    public String getMajor() {
+        return major;
+    }
 
-    }
-    public String getMajor(){
-        return Major;
-    }
-    public void setMajor(String Major){
-        this.Major = Major;
-    }
-    public String getGender(){
+    public String getGender() {
         return gender;
     }
-    public void setGender(String gender){
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public void setGender(String gender) {
         this.gender = gender;
     }
-    public void StudentInfo(){
-        System.out.println("please enter your name");
-        Scanner sc=new Scanner(System.in);
-        String Name=sc.nextLine();
-        System.out.println("please enter your address");
-        String Address=sc.nextLine();
-        System.out.println("please enter your email");
-        String Email=sc.nextLine();
-        System.out.println("please enter your ID");
-        int ID=sc.nextInt();
-        System.out.println("please enter your age");
-        if(age<0) {
-            System.out.println("invalid age");
-        }
-else {
-        int age=sc.nextInt();}
-        System.out.println("please enter your Major");
-        String Major=sc.nextLine();
-        System.out.println("please enter your Gender");
-        String Gender=sc.nextLine();
 
 
 
-
-    }
-
-
-}
